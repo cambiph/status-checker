49c35c3b796f4b3060c3759fd5a9d9accf2e0166
Philippe Cambien <philippe.cambien@gmail.com>
2018-05-02 14:57:34 +0200

    Add extra urls and write to file

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const urls = [
     'https://www.delijn.be/nl/haltes/halte/104716/Antwerpen_Premetrostation_Diamant',
+    'https://www.delijn.be/nl/haltes/halte/104716/Antwerpen_Premetrostation_Diamant#2',
+    'https://www.delijn.be/nl/lijnen/lijn/1/500/6/500_Mechelen_-_Rumst_-_Boom_-_Antwerpen',
+    'https://www.delijn.be/nl/lijnen/lijn/1/500/6/500_Mechelen_-_Rumst_-_Boom_-_Antwerpen#dienstregeling',
+    'https://www.delijn.be/nl/lijnen/lijn/1/500/6/500_Mechelen_-_Rumst_-_Boom_-_Antwerpen#omleidingen',
+    'https://www.delijn.be/nl/routeplanner/',
     'https://www.delijn.be'
 ]
 
@@ -10,13 +16,15 @@ urls.forEach(function (url) {
         try {
             const browser = await puppeteer.launch({
                 headless: true,
-                dumpio: true
+                dumpio: false
             });
             const page = await browser.newPage();
 
             page.on('response', response => {
                 if (response.status() != 200) {
-                    console.log('Failed!');
+                    fs.appendFileSync('result.csv', new Date().toISOString() + ',' + response.url() + ',' + response.status() + '\n' , (err) => {
+                        if (err) throw err;
+                    });
                 }
             });
 
