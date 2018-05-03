fb2f59cee4889cdef8eab74b791938d77dd1d92a
Philippe Cambien <philippe.cambien@gmail.com>
2018-05-03 13:15:19 +0200

    Refactoring

--- main.js
+++ main.js
@@ -7,35 +7,56 @@ const urls = [
     'https://www.delijn.be/nl/lijnen/lijn/1/500/6/500_Mechelen_-_Rumst_-_Boom_-_Antwerpen',
     'https://www.delijn.be/nl/lijnen/lijn/1/500/6/500_Mechelen_-_Rumst_-_Boom_-_Antwerpen#dienstregeling',
     'https://www.delijn.be/nl/lijnen/lijn/1/500/6/500_Mechelen_-_Rumst_-_Boom_-_Antwerpen#omleidingen',
-    'https://www.delijn.be/nl/routeplanner/',
-    'https://www.delijn.be'
+    'https://www.delijn.be/nl/routeplanner',
+    'https://www.delijn.be/nl'
 ]
 
-urls.forEach(function (url) {
-    (async () => {
-        try {
-            const browser = await launch({
-                dumpio: true
+async function launchBrowser() {
+    
+    let browser = await launch({dumpio: false, headless: false});
+    return browser;
+
+}
+
+async function setBrowser(url, browsder) {
+    
+    let page = await browser.newPage();
+
+    page.on('response', response => {
+        if (response.url().match('(https:)\D{2}[a-z]*.(delijn)(\S+)') && response.status() != 200) {
+            appendFileSync('result.csv', new Date().toISOString() + ',' + response.url() + ',' + response.status() + '\n', (err) => {
+                if (err) throw err;
             });
-
-            const page = await browser.newPage();
-
-            page.on('response', response => {
-                if(response.url().match('(https:)\D{2}[a-z]*.(delijn)(\S+)') && response.status() != 200) {
-                    appendFileSync('result.csv', new Date().toISOString() + ',' + response.url() + ',' + response.status() + '\n' , (err) => {
-                        if (err) throw err;
-                    });
-                }
-            });
-
-            await page.goto(url, {
-                waitUntil: 'networkidle0',
-                timeout: 120000
-            });
-
-            await browser.close();
-        } catch (error) {
-            console.log(error);
         }
-    })()
-});
+    });
+
+    return page;
+}
+
+async function performGet(page, url) {
+
+    await page.goto(url, {
+        waitUntil: 'networkidle0',
+        timeout: 120000
+    });
+
+}
+
+async function closeBrowser(browser) {
+    await browser.close();
+}
+
+urls.forEach(async (url) => {
+    let browser;
+    try {
+        browser = await launchBrowser();
+        const page = await setBrowser(url, browser);
+        await performGet(page, url);
+        await closeBrowser(browser);
+    } catch (error) {
+        console.log(error);
+        if (browser) {
+            await closeBrowser(browser);
+        }
+    }
+}); 
