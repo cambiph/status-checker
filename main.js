328d1f440b2ccf05523db275c0f389a12290ccbf
Philippe Cambien <philippe.cambien@gmail.com>
2018-05-03 16:12:35 +0200

    Add dynamic routeplanner url

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import { launch } from "puppeteer";
-import { appendFileSync } from "fs";
+import puppeteer from "puppeteer";
+import appendFileSync from "fs";
+import dateFormat from "dateformat";
 
-const urls = [
+let urls = [
     'https://www.delijn.be/nl/haltes/halte/104716/Antwerpen_Premetrostation_Diamant',
     'https://www.delijn.be/nl/haltes/halte/104716/Antwerpen_Premetrostation_Diamant#2',
     'https://www.delijn.be/nl/lijnen/lijn/1/500/6/500_Mechelen_-_Rumst_-_Boom_-_Antwerpen',
@@ -11,9 +12,44 @@ const urls = [
     'https://www.delijn.be/nl'
 ]
 
+async function getDate() {
+    return dateFormat('dd-mm-yyyy');
+}
+
+async function getTime() {
+    return dateFormat('HH:MM');
+}
+
+async function getRoutePlannerUrl() {
+    return  'https://www.delijn.be/nl/routeplanner/resultaten.html?' +
+            'from=Station+Antwerpen+Centraal+%5BB%5D' +
+            '&startXCoordinaat=153657' +
+            '&startYCoordinaat=211918' +
+            '&to=Station+Mechelen+%5BB%5D' + 
+            '&finishXCoordinaat=158001' +
+            '&finishYCoordinaat=189723' +
+            '&datum=' +  await getDate() +
+            '&departureChoice=1' +
+            '&tijd=' + await getTime() +
+            '&option-bus=on' +
+            '&option-tram=on' +
+            '&option-metro=on' +
+            '&option-trein=on' +
+            '&option-belbus=off';
+}
+
+async function addUrlToArray() {
+    await urls.push(await getRoutePlannerUrl());
+}
+
 async function launchBrowser() {
-    let browser = await launch({dumpio: true, headless: true});
-    return browser;
+    return await puppeteer.launch({dumpio: false, headless: true});
+}
+
+async function writeToFile(response) {
+    appendFileSync('result.csv', new Date().toISOString() + ',' + response.url() + ',' + response.status() + '\n', (err) => {
+        if (err) throw err;
+    });
 }
 
 async function setBrowser(url, browser) {
@@ -21,10 +57,8 @@ async function setBrowser(url, browser) {
 
     page.on('response', response => {
         if (!response.url().includes('google') && response.status() != 200) {
-            appendFileSync('result.csv', new Date().toISOString() + ',' + response.url() + ',' + response.status() + '\n', (err) => {
-                if (err) throw err;
-            });
-        }
+            writeToFile(response);
+        };
     });
 
     return page;
@@ -42,7 +76,7 @@ async function closeBrowser(browser) {
     await browser.close();
 }
 
-urls.forEach(async (url) => {
+async function testUrl(url) {
     let browser;
     try {
         browser = await launchBrowser();
@@ -55,4 +89,19 @@ urls.forEach(async (url) => {
             await closeBrowser(browser);
         }
     }
-}); 
+}
+
+async function run() {
+    await addUrlToArray();
+    
+    urls.forEach(async (url) => {
+        try {
+            console.log('Testing ' + url)
+            await testUrl(url);
+        } catch (err) {
+            console.log(err);
+        }
+    }); 
+}
+
+run();
